5e11568b0885b8ba76418a67516750aa5a778742
dushyant-appinventiv <dushyant.goyal@appinventiv.com>
2021-09-06 12:14:29 +0530

    updated UI and routes, used HashRouter

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
 import Home from "./screens/Home";
 import TopNav from "./components/topNav";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./components/footer";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Categories from "./screens/Categories";
 import SubCategories from "./screens/SubCategories";
 import CustomBread from "./components/breadCrumbs";
-import { BottomNavigation, Container, Typography } from "@material-ui/core";
+import { Container } from "@material-ui/core";
+
+const mainContainerStyle = {
+  position: "relative",
+  height: "90vh",
+  overflowX: "hidden",
+  display: "flex",
+  alignItems: "center",
+};
 
 function App() {
-  const date = new Date().toDateString();
-  const footerStyle = {
-    width: "100%",
-    marginTop: "10%",
-    display: "flex",
-    alignItems: "center",
-  };
   return (
-    <div style={{ position: "relative" }}>
-      <Router>
+    <>
+      <HashRouter>
         <TopNav />
-        <Switch>
-          <Route path={"/"} exact component={Home} />
-          <>
+        <Container style={{ ...mainContainerStyle }}>
+          <Switch>
+            <Route path={"/"} exact component={Home} />
             <Container fluid={"md"}>
               <CustomBread />
               <Route path={"/categories"} component={Categories} />
               <Route path={"/subcategories"} component={SubCategories} />
             </Container>
-          </>
-        </Switch>
-      </Router>
-      <BottomNavigation style={{ ...footerStyle }}>
-        <Typography
-          align={"center"}
-          variant={"subtitle1"}
-          color={"textSecondary"}>
-          {"Copyright : Dushyant Goyal Date: " + date}
-        </Typography>
-      </BottomNavigation>
-    </div>
+          </Switch>
+          <Footer />
+        </Container>
+      </HashRouter>
+    </>
   );
 }
 
