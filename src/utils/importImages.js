99ca7948dace4548900609f51250627bbca3cd2d
dushyant-appinventiv <dushyant.goyal@appinventiv.com>
2021-09-06 12:17:48 +0530

    added file having image imports & data dir

---
+++ src/utils/importImages.js
@@ -0,0 +1,154 @@
+// importing categories
+import booms from "../assets/images/category/booms.png";
+import carry_deck_crane from "../assets/images/category/carry_deck_crane.png";
+import earth_moving from "../assets/images/category/earth_moving.png";
+import forklifts from "../assets/images/category/forklifts.png";
+import material_lifts from "../assets/images/category/material_lifts.png";
+import personnel_cart from "../assets/images/category/personnel_cart.png";
+import scissor_lifts from "../assets/images/category/scissor_lifts.png";
+import single_man_lifts from "../assets/images/category/single_man_lifts.png";
+import telehandlers from "../assets/images/category/telehandlers.png";
+
+// importing subcategories
+import boomsAElectric from "../assets/images/category/subcategory/booms_articulated_electric.png";
+import boomsAEngine from "../assets/images/category/subcategory/booms_articulated_engine.png";
+import boomsStraight from "../assets/images/category/subcategory/booms_straight.png";
+import boomsTowable from "../assets/images/category/subcategory/booms_towable.png";
+import earthmovingBackhoe from "../assets/images/category/subcategory/earth_moving_backhoe.png";
+import earthmovingLoader from "../assets/images/category/subcategory/earth_moving_compact_truck_loader.png";
+import earthmovingExcavator from "../assets/images/category/subcategory/earth_moving_mini_excavator.png";
+import earthmovingSteer from "../assets/images/category/subcategory/earth_moving_skid_steer.png";
+import forkliftElectric from "../assets/images/category/subcategory/forklift_electric.png";
+import forkliftPneumatic from "../assets/images/category/subcategory/forklift_ic_pneumatic.png";
+import forkliftTerrain from "../assets/images/category/subcategory/forklift_rough_terrain.png";
+import forkliftSwingmast from "../assets/images/category/subcategory/forklift_swingmast.png";
+import forkliftTugger from "../assets/images/category/subcategory/forklift_tugger.png";
+import forkliftCushion from "../assets/images/category/subcategory/forklifts_ic_cushion.png";
+import forkliftPiggyback from "../assets/images/category/subcategory/forklift_piggyback.png";
+import personnelTerrain from "../assets/images/category/subcategory/personnel_cart_rough_terrain.png";
+import scissorLiftTerrain from "../assets/images/category/subcategory/scissor_lift_rough_terrain.png";
+import scissorLiftSlab from "../assets/images/category/subcategory/scissor_lift_slab.png";
+import singleManDriveable from "../assets/images/category/subcategory/single_man_lifts_driveable.png";
+import singleManPushAround from "../assets/images/category/subcategory/single_man_lifts_push_around.png";
+
+export const categoryImages = [
+  {
+    imgSrc: booms,
+    name: "booms.png",
+  },
+  {
+    imgSrc: carry_deck_crane,
+    name: "carry_deck_crane.png",
+  },
+  {
+    imgSrc: earth_moving,
+    name: "earth_moving.png",
+  },
+  {
+    imgSrc: forklifts,
+    name: "forklifts.png",
+  },
+  {
+    imgSrc: material_lifts,
+    name: "material_lifts.png",
+  },
+  {
+    imgSrc: personnel_cart,
+    name: "personnel_cart.png",
+  },
+  {
+    imgSrc: scissor_lifts,
+    name: "scissor_lifts.png",
+  },
+  {
+    imgSrc: single_man_lifts,
+    name: "single_man_lifts.png",
+  },
+  {
+    imgSrc: telehandlers,
+    name: "telehandlers.png",
+  },
+];
+
+export const subCategoryImages = [
+  {
+    imgSrc: boomsAElectric,
+    name: "booms_articulated_electric.png",
+  },
+  {
+    imgSrc: boomsAEngine,
+    name: "booms_articulated_engine.png",
+  },
+  {
+    imgSrc: boomsStraight,
+    name: "booms_straight.png",
+  },
+  {
+    imgSrc: boomsTowable,
+    name: "booms_towable.png",
+  },
+  {
+    imgSrc: earthmovingBackhoe,
+    name: "earth_moving_backhoe.png",
+  },
+  {
+    imgSrc: earthmovingLoader,
+    name: "earth_moving_compact_truck_loader.png",
+  },
+  {
+    imgSrc: earthmovingExcavator,
+    name: "earth_moving_mini_excavator.png",
+  },
+  {
+    imgSrc: earthmovingSteer,
+    name: "earth_moving_skid_steer.png",
+  },
+  {
+    imgSrc: forkliftElectric,
+    name: "forklift_electric.png",
+  },
+  {
+    imgSrc: forkliftPneumatic,
+    name: "forklift_ic_pneumatic.png",
+  },
+  {
+    imgSrc: forkliftTerrain,
+    name: "forklift_rough_terrain.png",
+  },
+  {
+    imgSrc: forkliftSwingmast,
+    name: "forklift_swingmast.png",
+  },
+  {
+    imgSrc: forkliftTugger,
+    name: "forklift_tugger.png",
+  },
+  {
+    imgSrc: forkliftCushion,
+    name: "forklift_ic_cushion.png",
+  },
+  {
+    imgSrc: forkliftPiggyback,
+    name: "forklift_piggyback.png",
+  },
+  {
+    imgSrc: personnelTerrain,
+    name: "personnel_cart_rough_terrain.png",
+  },
+  {
+    imgSrc: scissorLiftTerrain,
+    name: "scissor_lift_rough_terrain.png",
+  },
+  {
+    imgSrc: scissorLiftSlab,
+    name: "scissor_lift_slab.png",
+  },
+  {
+    imgSrc: singleManDriveable,
+    name: "single_man_lifts_driveable.png",
+  },
+  {
+    imgSrc: singleManPushAround,
+    name: "single_man_lifts_push_around.png",
+  },
+];
